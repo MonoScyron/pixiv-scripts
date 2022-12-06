af0a583f3cf5b5485cd8801754a1518e104dd354
Zhiqin He <110511317+MonoScyron@users.noreply.github.com>
2022-12-05 20:25:17 -0500

    Commit already written code
    LikeLoveShortkey already finished.
    ArtistHeader and BookmarkEdit not started yet.

---
+++ bookmark_edit_shortkeys.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Pixiv Edit Bookmark Shortkeys
+// @namespace    https://github.com/MonoScyron/PixivScripts
+// @version      0.1
+// @description  Adds several shortkeys to the edit bookmark page + binds ctrl-enter to save & return to artwork
+// @author       MonoScyron
+// @updateURL    https://github.com/MonoScyron/PixivScripts/bookmark_edit_shortkeys.js
+// @downloadURL  https://github.com/MonoScyron/PixivScripts/bookmark_edit_shortkeys.js
+// @match        https://www.pixiv.net/bookmark_add*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.addEventListener("keydown", onkeydown);
+
+    onkeydown = function(e) {
+        if(e.ctrlKey && e.key == 'Enter') {
+            // TODO: Handle save bookmarks & return to artwork (instead of going to bookmarks page)
+        }
+        else if(e.key == 'ArrowLeft') {
+            // TODO: Handle move tag highlight one to the left. If no tag highlighted, highlight first tag
+        }
+        else if(e.key == 'ArrowRight') {
+            // TODO: Handle move tag highlight one to the right. If no tag highlighted, highlight first tag
+        }
+        else if(e.key == 'Enter') {
+            // TODO: Handle clicking highlighted tag
+        }
+    }
+})();
---
+++ like-love_shortkey.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Pixiv Like-Love Shortkey
+// @namespace    https://github.com/MonoScyron/PixivScripts
+// @version      1.1
+// @description  Links the "like" and "bookmark" button to ctrl-s (Private bookmark)
+// @author       MonoScyron
+// @updateURL    https://github.com/MonoScyron/PixivScripts/like-love_shortkey.js
+// @downloadURL  https://github.com/MonoScyron/PixivScripts/like-love_shortkey.js
+// @match        https://www.pixiv.net/en/artworks/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.addEventListener("keydown", onkeydown);
+
+    onkeydown = function(e) {
+        if(e.ctrlKey && e.key == 's') {
+            e.preventDefault();
+
+            var likeBtn = document.querySelector("button._35vRH4a");
+            if(likeBtn.getAttribute("disabled") == null) {
+                console.log("Accessing menu...");
+
+                document.querySelector("button.l6avS_e._1W1yk-M").click();
+
+                var del1 = setInterval(() => {
+                    var loveBtn = document.querySelector("li.sc-1o8nozx-2.bmbKoQ");
+                    if(likeBtn != null && loveBtn != null && loveBtn.getAttribute("disabled") != "" && likeBtn.getAttribute("disabled") != "") {
+                        console.log("Liking and bookmarking...");
+
+                        clearInterval(del1);
+                        loveBtn.click();
+                        likeBtn.click();
+
+                        var del2 = setInterval(() => {
+                            var editBtn = document.querySelector("a.sc-d98f2c-0.sc-1ij5ui8-1.RICfj");
+                            if(editBtn != null) {
+                                console.log("Editing bookmark...");
+
+                                clearInterval(del2);
+                                editBtn.click();
+                            }
+                        }, 10);
+                    }
+                }, 10);
+
+            }
+        }
+    }
+})();
