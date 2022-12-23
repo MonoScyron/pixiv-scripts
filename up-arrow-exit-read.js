1813e65aa08dc1374d470d25ebfc64938ff8f23b
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2022-12-23 03:06:14 -0500

    Fix up arrow exiting at 2nd page again

--- up-arrow-exit-read.js
+++ up-arrow-exit-read.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixiv Up Arrow Exit Read
 // @namespace    https://github.com/MonoScyron/pixiv-scripts
-// @version      1.0.2
+// @version      1.1.2
 // @description  Binds up arrow to exit "reading works" when at page 1 of "reading works".
 // @author       MonoScyron
 // @updateURL    https://raw.githubusercontent.com/MonoScyron/pixiv-scripts/main/up-arrow-exit-read.js
@@ -17,18 +17,22 @@
     var exit = false;
 
     document.addEventListener("keydown", (e) => {
-        if(window.location.href.indexOf("#") >= 0 && e.key == "ArrowUp") {
-            var val = document.querySelector("input.gtm-manga-viewer-change-page");
-            if(val.getAttribute("value") == "1") {
-                if(exit) {
-                    exit = false;
-                    document.querySelector("div.gtm-manga-viewer-close-icon").click();
+        if(window.location.href.indexOf("#") >= 0) {
+            if(e.key == "ArrowUp") {
+                var val = document.querySelector("div.sc-1oz5uvo-0.bFxHZI");
+                if(val.scrollTop == 0) {
+                    if(exit) {
+                        exit = false;
+                        document.querySelector("div.gtm-manga-viewer-close-icon").click();
+                    }
+                    else
+                        exit = true;
                 }
                 else
-                    exit = true;
+                    exit = false;
             }
             else
-                exit = false;
+                exit = false
         }
     });
 })();
