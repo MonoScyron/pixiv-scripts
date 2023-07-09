2ae6026aa7db119113e26ee78ad4cfc9a43e315c
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2023-07-08 22:17:47 -0700

    Wugh

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 Personal scripts for browsing pixiv.
 
 ## Bookmark Edit Shortkeys
-Adds shortkeys to the edit bookmark page.
+Adds shortkeys to the edit bookmark page and causes it to return to the artwork instead of your bookmarks.
+
 #### Saving and Removing Bookmarks
 - Ctrl-enter to save & return to artwork
 - Esc to remove bookmark & return to artwork
+
 #### Navigation of "Edit Bookmark" page
 - Hit enter/left arrow/right arrow to begin navigating tag list
 - Arrow keys to navigate tag list
@@ -16,8 +18,11 @@ Adds shortkeys to use while browsing works.
 - Links the "like" and "bookmark" button to alt-ctrl-s (Private bookmark), automatically redirects to edit the bookmark
 - Links "like" only to ctrl-s
 
-## Show Alt on Hover
-Adds the alt of works as a tooltip to their previews.
-
 ## Up Arrow Exit Read
 Binds up arrow to exit "reading works" when at page 1 of "reading works".
+
+## Show Alt on Hover (WIP)
+Adds the alt of works as a tooltip to their previews.
+
+## Browse by bookmark (TODO)
+Allows saved bookmarks to be browsed with arrow keys by public/private visibility and by selected tag
--- show-alt-on-hover.js
+++ show-alt-on-hover.js
@@ -15,180 +15,63 @@
 // @noframes
 // ==/UserScript==
 
-const tooltipStyleCSS = `
-/* Position tooltip text */
-.tooltiptext-over {
-    bottom: 110%;
-}
-
-.tooltiptext-under {
-    top: 110%;
-}
-
-.tooltiptext-overlay {
-    top: 15%;
-}
-
-/* General formatting for tooltip text */
-.tooltiptext {
-    display: inline-block;
-    visibility: hidden;
-    width: 145%;
-    background-color: #555;
-    color: #fff;
-    text-align: center;
-    padding: 3px;
-    border-radius: 6px;
-    z-index: 1;
-
-    /* Position tooltip */
-    position: absolute;
-    left: -25%;
-
-    /* Fade in tooltip */
-    opacity: 0;
-    transition: opacity 0.1s;
-}
-
-/* General formatting for tooltip */
-.tooltip {
-    position: relative;
-}
-
-/* Show the tooltip text when you mouse over the tooltip container */
-.tooltip:hover .tooltiptext {
-    visibility: visible;
-    opacity: 1;
-}
-`;
-
 (function() {
     'use strict';
 
     // ? Consider injecting alt directly to preview (no need to hover to show)
     // ? Consider splitting this script into multiple, one for each matched page (if too difficult to scrape w/ 1 algo)
 
-    // Inject tooltip style into document
-    const tooltipStyle = document.createElement('style');
-    tooltipStyle.id = "userscript-tooltip-style";
-    tooltipStyle.innerHTML = tooltipStyleCSS;
-    document.head.appendChild(tooltipStyle);
+    console.log("Script: Loaded")
 
-    var root = document.querySelector("div#root");
+    let root = document.querySelector("div#root");
     if(root != null) {
-        const config = { attributes: false, childList: true, subtree: true };
+        console.log("Script: Begin observing mutations")
+        const config = {attributes: false, childList: true, subtree: true};
         const observer = new MutationObserver(callback);
         observer.observe(root, config);
     }
 
     /**
      * Callback function to execute when mutations are observerd.
-     * @param {Array<MutationRecord>} mList 
-     * @param {MutationObserver} observer 
-    */
+     * @param {Array<MutationRecord>} mList
+     * @param {MutationObserver} observer
+     */
     function callback(mList, observer) {
         for(const m of mList) {
-            if(m.type == "childList") {
+            if(m.type === "childList") {
                 // ! No need to add alt of novels (Thankfully pixiv doesn't mix the two)
 
-                var target = m.target;
+                let target = m.target;
 
                 // On header load, scrape alt of header works and add them as tooltips to the work previews
-                if(target.nodeName == "ASIDE" && target.classList.contains("sc-1nr368f-7") && target.classList.contains("hdvpLU")) {
-                    console.log("Script: Adding alts to header...");
+                // TODO: Script tries to grab alts too early and gets null
+                if(target.nodeName === "ASIDE" && target.classList.contains("sc-1nr368f-7") && target.classList.contains("hdvpLU")) {
+                    console.log(`Script: Adding alts to header...`);
                     target.querySelector("nav.sc-x0j4pn-0.gbvtwZ").childNodes.forEach((n) => {
-                        addTooltipUnder(n);
+                        addTitle(n);
                     });
                 }
 
                 // On footer load, scrape alt of footer works and add them as tooltips to the work previews
-                // TODO: Stop footer nav container from automatically expanding when adding alt tooltips
-                if(target.nodeName == "DIV" && target.classList.contains("sc-rp5asc-9") && target.classList.contains("cYUezH")) {
+                if(target.nodeName === "DIV" && target.classList.contains("sc-rp5asc-9") && target.classList.contains("cYUezH")) {
                     console.log("Script: Adding alts to a footer work...");
-                    // * This doesn't work yet!
-                    // addTooltipUnder(target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
                 }
-
                 // TODO: Scrape alt of /tags/* (works search) page and add them as tooltips to the work previews
                 // TODO: Scrape alt of /users/* (users) page and add them as tooltips to the work previews
                 // TODO: Scrape alt of /*/ (home) page and add them as tooltips to the work previews
             }
         }
-    }
 
-    /**
-     * Takes a work preview and overlays its alt attribute as a tooltip over the preview.
-     * @param {HTMLDivElement} workPreview Preview of artwork in footer
-     */
-    function addTooltipOverlay(workPreview) {
-        const img = getImageChild(workPreview);
-        if(img == null)
-            return;
-
-        const alt = img.getAttribute("alt");
-        if(alt != null) {
-            const tooltip = document.createElement('span');
-            tooltip.className = "tooltiptext tooltiptext-overlay";
-            tooltip.innerHTML = alt;
-
-            workPreview.classList.add("tooltip");
-            workPreview.appendChild(tooltip);
+        /**
+         * @param {HTMLElement} n
+         */
+        function addTitle(n) {
+            n = n.querySelector('img');
+            if(n != null) {
+                let alt = n.getAttribute("alt");
+                console.log(`Script: ${alt}`)
+                n.setAttribute("title", alt);
+            }
         }
-    }
-
-    /**
-     * Takes a work preview and adds its alt attribute as a tooltip above it.
-     * @param {HTMLDivElement} workPreview Preview of artwork in footer
-     */
-    function addTooltipAbove(workPreview) {
-        const img = getImageChild(workPreview);
-        if(img == null)
-            return;
-
-        const alt = img.getAttribute("alt");
-        if(alt != null) {
-            const tooltip = document.createElement('span');
-            tooltip.className = "tooltiptext tooltiptext-over";
-            tooltip.innerHTML = alt;
-
-            workPreview.classList.add("tooltip");
-            workPreview.appendChild(tooltip);
-        }
-    }
-
-    /**
-     * Takes a work preview and adds its alt attribute as a tooltip under it.
-     * @param {HTMLDivElement} workPreview Preview of artwork in header
-     */
-    function addTooltipUnder(workPreview) {
-        if(workPreview.childNodes.length >= 2)
-            return;
-
-        const img = getImageChild(workPreview);
-        if(img == null)
-            return;
-
-        const alt = img.getAttribute("alt");
-        if(alt != null) {
-            const tooltip = document.createElement('span');
-            tooltip.className = "tooltiptext tooltiptext-under";
-            tooltip.innerHTML = alt;
-
-            workPreview.classList.add("tooltip");
-            workPreview.appendChild(tooltip);
-        }
-    }
-
-    /**
-     * Takes an html element and searches through its first child elements until an img element is found.
-     * @param {HTMLElement} element Element to get img child element from
-     * @returns HTMLImageElement or null if no img element is found
-     */
-    function getImageChild(element) {
-        var ret = element;
-        while(ret != null && ret.nodeName != 'IMG') {
-            ret = ret.firstChild;
-        }
-        return ret;
     }
 })();
